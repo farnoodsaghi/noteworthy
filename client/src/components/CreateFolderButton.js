aedb98330ec8f1f657e24688f14a6729c6109b65
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-07-29 09:23:14 +0800

    feat: change button to div and replace its innerHTML with icon

--- client/src/components/CreateFolderButton.js
+++ client/src/components/CreateFolderButton.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useGlobalContext } from "../context";
+import { Icon } from "@iconify/react";
+import "./create-folder-button.scss";
 
 const CreateFolderButton = () => {
   const { setIsModalVisible } = useGlobalContext();
 
-  return <button onClick={() => setIsModalVisible(true)}>Create Folder</button>;
+  return (
+    <div className="sidebar-three-dots" onClick={() => setIsModalVisible(true)}>
+      <Icon icon="bi:three-dots-vertical" />
+    </div>
+  );
 };
 
 export default CreateFolderButton;
