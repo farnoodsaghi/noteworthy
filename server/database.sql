27b95161879f1054e479197992f354aaef497c0f
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-08-13 01:02:33 +0800

    fix: update users and notes table schemas

--- server/database.sql
+++ server/database.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE noteworthy;
 
 CREATE TABLE notes (
-    id UUID NOT NULL PRIMARY KEY, 
+    note_id UUID NOT NULL PRIMARY KEY,
+    user_id UUID NOT NULL,
+    folder_id UUID NOT NULL,
     title TEXT,
     content TEXT,
-    folder_id UUID NOT NULL,
-    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
 );
 
 CREATE TABLE users(
     user_id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(255) NOT NULL,
 );
